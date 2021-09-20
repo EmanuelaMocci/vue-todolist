37570492382cd9f0c2795f4285bacf078421c916
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-09-20 16:17:18 +0200

    Aggiunta logo, input e button

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// - stampare in pagina un item per ogni elemento contenuto in un array
+// - ogni item ha una "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// - predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+
+var app = new Vue({
+    el: '#root',
+    data: {
+        logo: 'img/logo.png'
+    },
+    methods: {
+
+    }
+})
